37361efc17665ad4277d0120548222908195bc0b
Chetan Sachdev <cksachdev@gmail.com>
2013-08-15 17:43:38 +0530

    Initial commit
    First commit for #golang workshop

---
+++ beginner/code/03.2-branchingswitch.go
@@ -0,0 +1,15 @@
+package main
+import ("fmt", "time")
+func main() {
+    i := 2
+    fmt.Print("write ", i, " as ")
+    switch i {
+    case 1:
+        fmt.Println("one")
+    case 2:
+        fmt.Println("two")
+    case 3:
+        fmt.Println("three")
+    }
+    
+}
